001de3eb435c035b09b2054b9bbb74f5ebfda2cd
Emre Ulgen <emreulgen443@gmail.com>
2026-08-31 12:59:28 +0300

    list & tuples

--- starting.hs
+++ starting.hs
@@ -1,1 +1,10 @@
+modulus = [ x | x <- [50..100], x `mod` 7 == 3]
+
+notEqual = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19] 
+
+first = fst (8,11)  
+second = snd ("Wow", False)
+
+zipping = zip [1,2,3,4] ['E','M','R','E']
+
 rightTriangles =[ (a,b,c) | c <- [1..50], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
